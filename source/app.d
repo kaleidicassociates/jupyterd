5777e4a2bd85427bf1c0a7c11bb31030c164c595
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2018-07-19 14:57:47 +0800

    app.d

---
+++ source/app.d
@@ -0,0 +1,21 @@
+import juypterd.interpreter;
+import juypterd.kernel;
+import zmqd;
+import std.stdio;
+
+int main(string[] args)
+{
+    Interpreter i;
+    writeln(zmqVersion());
+    switch(args[1])
+    {
+        case "echo":
+            i = new EchoInterpreter();
+            break;
+        default:
+            return 1;
+    }
+    auto k = Kernel(i,/*connection string=*/args[2]);
+    k.mainloop();
+    return 0;
+}
